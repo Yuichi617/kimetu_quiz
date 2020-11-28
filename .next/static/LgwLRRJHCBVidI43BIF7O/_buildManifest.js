04ac53d37f22c168d499c09fb88f82e480a73efc
Yuichi <ozwald176@gmail.com>
2020-11-28 16:14:56 +0900

    second

---
+++ .next/static/LgwLRRJHCBVidI43BIF7O/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-e340fc7c52b29689851b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-185f74e6c80a35b01ffd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+import Layout from '../components/Layout';
+
+export default () =>(
+    <Layout header="Next" title="Top page.">
+        Hello World
+    </Layout>
+);
